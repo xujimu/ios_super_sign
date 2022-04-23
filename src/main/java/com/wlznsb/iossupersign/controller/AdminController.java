6ef7fcd508e4610bb1c6d8ff7dcf95558ef13fd2
xujimu <2524931333@qq.com>
2022-04-23 11:21:19 +0800

    修复超级签mdm下载为灰色问题

--- src/main/java/com/wlznsb/iossupersign/controller/AdminController.java
+++ src/main/java/com/wlznsb/iossupersign/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.wlznsb.iossupersign.controller;
 
+import cn.hutool.core.io.FileUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
